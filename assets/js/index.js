9d29259a6aec53bef895e67ee7357b7f0caffb04
Ilya G <ilya.golyashov@yandex.ru>
2023-04-06 18:58:10 +0300

    06/04 end work-day

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,9 +1,34 @@
+// Моб меню
+function mobNav () {
+    let navbarToggler = document.querySelector(".nav-burger"); 
+    let myNavbar = document.querySelector(".nav");
+    let burgerPic = document.querySelector(".burger-pic");
 
-let navbarToggler = document.querySelector(".nav-burger");
-let myNavbar = document.querySelector(".nav");
-let burgerPic = document.querySelector(".burger-pic");
+    navbarToggler.onclick = function () {
+        myNavbar.classList.toggle("nav--active"); /* открытие меню */
+        burgerPic.classList.toggle("burger-pic--open"); /* смена бургера на крестик */
+    }
+};
 
-navbarToggler.onclick = function () {
-    myNavbar.classList.toggle("nav--active");
-    burgerPic.classList.toggle("burger-pic--open");
+function secondSlider() {
+    let slides = document.querySelectorAll('.second-slider__slide');
+  
+    slides.forEach(function (item) {
+        item.onclick = function() {
+            if (!item.classList.contains('second-slider__slide--active')) {
+                slides.forEach(function (slide) {
+                    slide.classList.remove('second-slider__slide--active');
+                });
+            item.classList.add('second-slider__slide--active');
+            }
+        };
+    });
 }
+  
+
+
+
+
+mobNav();
+secondSlider();
+
